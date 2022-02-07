52aea1ca65ee297f7874b825f5ee709d976db5b4
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-02-07 19:02:20 +0530

    added mapping

--- contracts/ShanToken.sol
+++ contracts/ShanToken.sol
@@ -5,7 +5,10 @@ contract ShanToken{
     // This value store number of Tokens to be made avaialble for this Smart Contract 
     uint256 public totalSupply;
 
+    mapping (address => uint256) public balanceOf;
+
      constructor(uint256 _totalSupply ){
+         balanceOf[msg.sender] = _totalSupply;
         totalSupply = _totalSupply;
     }
 }
--- test/ShanToken.js
+++ test/ShanToken.js
@@ -1,12 +1,16 @@
 var ShanToken = artifacts.require("../contracts/ShanToken.sol");
 
 contract('ShanToken', function(accounts){
+    var tokenInstance;
     it('sets total supply upon deployment', function(){
         return ShanToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
            assert.equal (totalSupply.toNumber(), 100000, 'sets the total supply to 100000')
+           return tokenInstance.balanceOf(accounts[0]);
+        }).then(function (adminBalance) {
+            assert.equal(adminBalance.toNumber(), 100000, 'it allocates the initial supply to admin account');
         });
     });
-})
+});
